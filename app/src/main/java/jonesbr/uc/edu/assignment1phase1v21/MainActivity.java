bb1830973f476f53d6826ff806571dd9c4e18f6f
jonesbr <brandan.jones@uc.edu>
2016-09-17 19:20:21 -0400

    Initial commit

---
+++ app/src/main/java/jonesbr/uc/edu/assignment1phase1v21/MainActivity.java
@@ -0,0 +1,13 @@
+package jonesbr.uc.edu.assignment1phase1v21;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
